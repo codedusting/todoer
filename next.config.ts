5deb657dde6e1c8c8b3a868a8589d22901d1d963
Code Dusting <codedusting@gmail.com>
2025-01-09 19:31:24 +0000

    Commit 2: Add initial configs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    useLightningcss: true,
+    cssChunking: true,
+    webVitalsAttribution: ["LCP", "CLS", "INP"],
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [],
+  },
 };
 
 export default nextConfig;
