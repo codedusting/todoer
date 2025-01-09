3695408ff3c702639651b653e2ce7787332bf987
Code Dusting <codedusting@gmail.com>
2025-01-09 19:35:11 +0000

    Commit 4: Add winston logger configuration

---
+++ src/lib/winston.ts
@@ -0,0 +1,47 @@
+import * as winston from "winston";
+import * as fs from "fs";
+import { format } from "date-fns";
+import { toZonedTime } from "date-fns-tz";
+
+const timeZone = "Asia/Kolkata"; // IST timezone
+
+const logDir = "logs";
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const l = winston.createLogger({
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
+  transports: [
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "error",
+    }),
+    new winston.transports.Http({
+      level: "warn",
+      format: winston.format.json(),
+    }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  l.add(
+    new winston.transports.Console({
+      level: "info",
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.printf(({ level, message, timestamp }) => {
+          const zonedDate = toZonedTime(timestamp as string, timeZone);
+          return `${level}: ${message} on ${format(zonedDate, "yyyy-MM-dd@HH:mm:ss")}`;
+        }),
+      ),
+    }),
+  );
+}
+
+export default l;
